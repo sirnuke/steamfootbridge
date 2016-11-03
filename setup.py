8d034ca0c30166ec3972d0f8db83e00ff4f8055f
Bryan DeGrendel <bryan@degrendel.com>
2016-11-03 00:31:11 -0400

    Convert Python installation to PyPi
    This does mean python-pip or similar will need to be installed on the
    system.

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='steamfootbridge',
       version='0.0.1',
       packages=['steamfootbridge'],
       scripts=['bin/steamfootbridge'],
+      install_requires=[
+        'steamodd',
+        ],
 )
