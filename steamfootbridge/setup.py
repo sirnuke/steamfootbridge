8c5998b5905552494def49429175eb835dbe7cea
Bryan DeGrendel <bryan@degrendel.com>
2016-11-06 18:20:20 -0500

    Setup now sets userconfig settings
    Disables friends autologin and overlay for Wine Steam.

--- steamfootbridge/setup.py
+++ steamfootbridge/setup.py
@@ -5,9 +5,45 @@ from . import config
 
 import steam
 
+# TODO: Does capitalization mater?
+__root_userconfig_key__     = u'UserLocalConfigStore'
+__friends_userconfig_key__  = u'friends'
+__autologin_friends_key__   = u'AutoSignIntoFriends'
+__system_userconfig_key__   = u'system'
+__enable_game_overlay_key__ = u'EnableGameOverlay'
+
+# TODO: Option to disable setting friends autologin
+# TODO: Option to disable setting overlay disabled
+# TODO: Option to avoid touching the userconfig at all
 def do():
   with config.Configuration() as c:
     path = c.get_wine_steam_userconfig_filename()
-    print "Updating {}".format(path)
-    with open(path) as f:
+    print "Reading {}".format(path)
+
+    with open(path, 'r') as f:
       userconfig = steam.vdf.load(f)
+
+    _base_setup_userconfig(userconfig)
+    _set_disable_friends_auto_login(userconfig)
+    _set_disable_overlay(userconfig)
+
+    print "Writing updated {}".format(path)
+    with open(path, 'w') as f:
+      steam.vdf.dump(userconfig, f)
+
+def _base_setup_userconfig(userconfig):
+  if not __root_userconfig_key__ in userconfig:
+    userconfig[__root_userconfig_key__] = {}
+  root = userconfig[__root_userconfig_key__]
+
+  if not __friends_userconfig_key__ in root:
+    root[__friends_userconfig_key__] = {}
+
+  if not __system_userconfig_key__ in root:
+    root[__system_userconfig_key__] = {}
+
+def _set_disable_friends_auto_login(userconfig):
+  userconfig[__root_userconfig_key__][__friends_userconfig_key__][__autologin_friends_key__] = u'0'
+
+def _set_disable_overlay(userconfig):
+  userconfig[__root_userconfig_key__][__system_userconfig_key__][__enable_game_overlay_key__] = u'0'
